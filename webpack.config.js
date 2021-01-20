f2d094b6b255fba9cb7e801b228594795f063af6
slTrust <trustfor@sina.cn>
2021-01-20 11:41:42 +0800

    webpack.config分为了两份prod和 dev ,index.d.ts移到dist目录里

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,5 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
-  // mode: 'development',
-  mode: 'production',
   entry: {
     index: './lib/index.tsx'
   },
@@ -21,26 +18,5 @@ module.exports = {
         loader: 'awesome-typescript-loader'
       }
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'FUI  - React',
-      template: 'index.html'
-    })
-  ],
-  externals: {
-    // 四个格式是分别对应js历史上各种不同的打包工具的
-    react: {
-      commonjs: 'react',
-      commonjs2: 'react',
-      amd: 'react',
-      root: 'React'
-    },
-    'react-dom': {
-      commonjs: 'react-dom',
-      commonjs2: 'react-dom',
-      amd: 'react-dom',
-      root: 'ReactDom'
-    }
   }
 }
