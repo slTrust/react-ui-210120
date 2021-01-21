2c4df1a77b42199612ea4abeae858aa6393a5232
slTrust <trustfor@sina.cn>
2021-01-21 12:37:49 +0800

    添加 onClick 事件

--- lib/Icon/Icon.tsx
+++ lib/Icon/Icon.tsx
@@ -3,12 +3,13 @@ import '../importIcons';
 import './icon.scss'
 interface IconProps {
   name: string;
+  onClick: ()=> void
 }
 
 const Icon: React.FC<IconProps> = props => {
   return (
     <span>
-      <svg  className="fui-icon">
+      <svg  className="fui-icon" onClick={props.onClick}>
         <use xlinkHref={`#${props.name}`} />
       </svg>
     </span>
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Icon from './Icon/Icon';
+
+const fn = ()=>{
+    console.log('fn')
+}
 ReactDOM.render(
   (<div>
-      <Icon name="wechat" />
-      <Icon name="qq" />
-      <Icon name="alipay" />
+      <Icon name="wechat" onClick={fn} />
   </div>),
   document.querySelector('#root'));
