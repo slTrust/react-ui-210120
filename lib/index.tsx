621922180a1cc4969b77b547fb87e494b91b93d9
slTrust <trustfor@sina.cn>
2021-01-21 16:48:02 +0800

    npm publish -package.json里指定 "files":["/dist/**/*"],才能发布打包后的所有文件

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,23 +1,2 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Icon from './Icon/Icon';
+export {default as Icon} from './Icon/Icon';
 
-const fn:React.MouseEventHandler = (e)=>{
-    console.log(e.target)
-    // 强制断言
-    console.log((e.target as SVGUseElement).href)
-
-    // 这句话可能是错的～～～ 因为传递的元素 这里肯定不是div而是 svg
-    // console.log((e.target as HTMLDivElement).style)
-}
-ReactDOM.render(
-  (<div>
-      <Icon name="wechat"
-            className="aaa"
-            onClick={fn}
-            onMouseEnter={() => console.log('enter')}
-            onMouseLeave={() => console.log('leave')}
-            onTouchStart={() => console.log('touch')}
-      />
-  </div>),
-  document.querySelector('#root'));
