6759a53a35beb562ade60c97165a2199bad1b188
slTrust <trustfor@sina.cn>
2021-01-21 15:00:35 +0800

    icon测试onClick

--- lib/Icon/__tests__/icon.unit.jsx
+++ lib/Icon/__tests__/icon.unit.jsx
@@ -1,11 +1,18 @@
 import * as renderer from 'react-test-renderer'
 import React from 'react'
 import Icon from '../Icon.tsx'
+import {mount} from 'enzyme'
 
 describe('Icon', () => {
   it('是个 icon', () => {
     const json = renderer.create(<Icon name="alipay" />)
       .toJSON()
     expect(json).toMatchSnapshot()
+  })
+  it('onClick', () => {
+    const fn = jest.fn()
+    const component = mount(<Icon name="alipay" onClick={fn}/>)
+    component.find('svg').simulate('click')
+    expect(fn).toBeCalled()
   })
 })
