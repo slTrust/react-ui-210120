79fb832250452ed537913054b91cd7d56987b586
slTrust <trustfor@sina.cn>
2021-01-23 02:02:43 +0800

    完成 Layout 各个组件

--- lib/layout/layout.example.tsx
+++ lib/layout/layout.example.tsx
@@ -1,76 +1,58 @@
 import React from "react";
 import Layout from "./layout";
+import Header from "./header";
 import Content from "./content";
 import Footer from "./footer";
-import Header from "./header";
 import Aside from "./aside";
+import "./layout.example.scss";
 
-const Example: React.FC = () => {
+export default function() {
   return (
     <div>
       <div>
-        <h1>demo1</h1>
-        <div>
-          <Layout
-            className="hi"
-            style={{ height: "300px", background: "red", width: "500px" }}
-          >
-            <Header>header</Header>
-            <Content>content</Content>
-            <Footer>footer</Footer>
-          </Layout>
-        </div>
+        <h1>第一个例子</h1>
+        <Layout className="hi" style={{ height: 360, width: 500 }}>
+          <Header className="x">header</Header>
+          <Content className="y">content</Content>
+          <Footer className="x">footer</Footer>
+        </Layout>
       </div>
+
       <div>
-        <h1>demo2</h1>
-        <div>
-          <Layout
-            className="hi"
-            style={{ height: "300px", background: "red", width: "500px" }}
-          >
-            <Header>header</Header>
-            <Layout>
-              <Aside>aside</Aside>
-              <Content>content</Content>
-            </Layout>
-            <Footer>footer</Footer>
+        <h1>第二个例子</h1>
+        <Layout className="hi" style={{ height: 360, width: 500 }}>
+          <Header className="x">header</Header>
+          <Layout>
+            <Aside className="z">aside</Aside>
+            <Content className="y">content</Content>
           </Layout>
-        </div>
+          <Footer className="x">footer</Footer>
+        </Layout>
       </div>
+
       <div>
-        <h1>demo3</h1>
-        <div>
-          <Layout
-            className="hi"
-            style={{ height: "300px", background: "red", width: "500px" }}
-          >
-            <Header>header</Header>
-            <Layout>
-              <Content>content</Content>
-              <Aside>aside</Aside>
-            </Layout>
-            <Footer>footer</Footer>
+        <h1>第三个例子</h1>
+        <Layout className="hi" style={{ height: 360, width: 500 }}>
+          <Header className="x">header</Header>
+          <Layout>
+            <Content className="y">content</Content>
+            <Aside className="z">aside</Aside>
           </Layout>
-        </div>
+          <Footer className="x">footer</Footer>
+        </Layout>
       </div>
+
       <div>
-        <h1>demo4</h1>
-        <div>
-          <Layout
-            className="hi"
-            style={{ height: "300px", background: "red", width: "500px" }}
-          >
-            <Aside>aside</Aside>
-            <Layout>
-              <Header>header</Header>
-              <Content>content</Content>
-              <Footer>footer</Footer>
-            </Layout>
+        <h1>第四个例子</h1>
+        <Layout className="hi" style={{ height: 360, width: 500 }}>
+          <Aside className="z">aside</Aside>
+          <Layout>
+            <Header className="x">header</Header>
+            <Content className="y">content</Content>
+            <Footer className="x">footer</Footer>
           </Layout>
-        </div>
+        </Layout>
       </div>
     </div>
   );
-};
-
-export default Example;
+}
