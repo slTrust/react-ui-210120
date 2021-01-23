8aea28cdb56bb97c26a9c3589e115e50f8243431
slTrust <trustfor@sina.cn>
2021-01-23 13:07:10 +0800

    raw-loader 加载文件，显示源代码

--- lib/icon/icon.example.tsx
+++ lib/icon/icon.example.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Icon from "./icon";
+// !!raw-loader 代表开启自定义加载方式
+const x = require('!!raw-loader!./icon.example.tsx');
+console.log(x.default)
 const IconExample:React.FC = ()=>{
     return (
         <div>
             <Icon name="alipay"/>
             <Icon name="qq"/>
             <Icon name="wechat"/>
+            <pre>
+                {x.default}
+            </pre>
         </div>
     )
 }
