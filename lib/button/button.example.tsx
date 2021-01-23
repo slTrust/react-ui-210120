3afe1d0a64b33b8b267c1bca20f615e59eb2e926
slTrust <trustfor@sina.cn>
2021-01-23 13:53:02 +0800

    remove all files

--- lib/button/button.example.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Button from './button';
-const ButtonExample:React.FC = ()=>{
-    return (<Button>hi</Button>)
-}
-
-export default ButtonExample;
--- lib/button/button.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-const Button:React.FC = (props) =>{
-  return (
-    <button>{props.children}</button>
-  )
-}
-
-export default Button;
